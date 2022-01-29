039e82f519452187aa0ff04721bcbd59e4f1621b
Nickeron-dev <koshkin061969@gmail.com>
2022-01-29 23:19:30 +0200

    Did my homework.

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("HI!");
+
+Console.WriteLine("Input price of 1 copybook: ");
+int PriceOfOneCopybook = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input amount of copybooks: ");
+int AmountOfCopybooks = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input price of 1 pen: ");
+int PriceOfOnePen = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input amount of pens: ");
+int AmountOfPens = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Total: " + (PriceOfOneCopybook * AmountOfCopybooks
+    + PriceOfOnePen * AmountOfPens) + " hryvnias.");
