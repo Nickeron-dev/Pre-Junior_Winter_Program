8c469b25c2bb8054f324b40035778274c30d76a0
Nickeron-dev <koshkin061969@gmail.com>
2022-02-06 17:58:00 +0200

    Did my second homework in C#.

---
+++ C#/Homework2/Homework2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Доброго дня, введіть число грошей Петрика: ");
+int s = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть к-сть зошитів, які він придбав: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть к-сть ручок, які він придбав: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть вартість одного зошита: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть вартість однієї ручки: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (s >= (n * x + m * y))
+{
+    Console.WriteLine($"Так, Петрику вистачить грошей. Залишок: {(s - (n * x + m * y))}");
+}
+else
+{
+    Console.WriteLine("На жаль, у Петрика недостатньо коштів для покупки.");
+}
