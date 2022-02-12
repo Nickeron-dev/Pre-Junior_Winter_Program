f6387b32ccc454d3bf0f9fa16979e0aa86fd3822
Nickeron-dev <koshkin061969@gmail.com>
2022-02-12 21:23:53 +0200

    Did my 3rd homework in C#.

---
+++ C#/Homework3/Homework3/Homework3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Input a number: ");
+int value = Convert.ToInt32(Console.ReadLine());
+String value_to_string = Convert.ToString(value);
+int answer = 0;
+for (int i = 0; i < value_to_string.Length; i++)
+{
+    answer += value_to_string[i] - 48;
+}
+Console.WriteLine($"The sum is: {answer}");
