e96c50d56c100328ad501209caf05eb7a326cbbb
Nickeron-dev <koshkin061969@gmail.com>
2022-04-05 13:47:34 +0200

    Finished next JS homework.

--- JavaScript/Homework6/index.js
+++ JavaScript/Homework6/index.js
@@ -3,5 +3,28 @@ const tasks = [
     { text: 'Pick up Tom from airport', done: false },
     { text: 'Visit party', done: false },
     { text: 'Visit doctor', done: true },
-    { text: 'Buy meat', done: true },
+    { text: 'Buy meat', done: true }
 ];
+
+let list = document.getElementById('list-of-tasks');
+for (let i = 0; i < tasks.length; i++)
+{
+    let htmlliElement = document.createElement('li');
+    let inputElement = document.createElement("input");
+    inputElement.type = "checkbox";
+    inputElement.checked = tasks[i].done;
+    if (tasks[i].done)
+    {
+        inputElement.className = "list__item-checkbox list__item_done";
+        htmlliElement.className = "list__item list__item_done";
+    }
+    else
+    {
+        inputElement.className = "list__item-checkbox";
+        htmlliElement.className = "list__item";
+    }
+
+    htmlliElement.append(inputElement);
+    htmlliElement.appendChild(document.createTextNode(tasks[i].text));
+    list.appendChild(htmlliElement);
+}
